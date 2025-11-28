b354ded9c072d8f5018f91af385e2254cfc2acd5
FCS-jpg <alicraft6810@gmail.com>
2025-11-28 08:58:11 -0500

    Print types and length of various variables
    Added print statements to display types and length of variables.

--- 02_Day_Variables_builtin_functions/variables.py
+++ 02_Day_Variables_builtin_functions/variables.py
@@ -11,3 +11,15 @@ is_true = True
 is_light_on = True
 hobby1, hobby2, hobby3 = "Wrestling", "Coding", "Traveling"
 
+print(type(first_name))
+print(type(last_name))
+print(type(full_name))
+print(type(country))
+print(type(city))
+print(type(age))
+print(type(year))
+print(type(is_married))
+print(type(is_true))
+print(type(is_light_on))
+
+print(len(first_name))
