7468457735cf7bc993379d162dfd780b407b7924
FCS-jpg <alicraft6810@gmail.com>
2025-11-28 08:33:49 -0500

    Add variables for personal information and hobbies

--- 02_Day_Variables_builtin_functions/variables.py
+++ 02_Day_Variables_builtin_functions/variables.py
@@ -1,1 +1,13 @@
+#Day 2: 30 Days of python programming
+first_name = "Ali"
+last_name = "Makhmudov"
+full_name = first_name + last_name
+country = "Turkey"
+city = "Atlanta"
+age = 14
+year = 2011
+is_married = False
+is_true = True
+is_light_on = True
+hobby1, hobby2, hobby3 = "Wrestling", "Coding", "Traveling"
 
